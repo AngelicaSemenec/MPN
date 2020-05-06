5e392abb95b8afe575cded033c7aac620fbc2f3f
AngelicaSemenec <48040875+AngelicaSemenec@users.noreply.github.com>
2020-05-06 13:41:57 -0700

    Updated UI

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ from resources import get_bucket, get_bucket_list
 
 from video import Video
 
+from flask_pymongo import PyMongo 
+from datetime import datetime
+import bcrypt
+
+import cv2
+import time
+from motion_detection import main_motion_detection
+from no_detection import main_no_detection
+from no_detection2 import main_no_detection2
+from no_detection3 import main_no_detection3
+from facial_detection import main_facial_detection
+
 #Create Flask instance
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,50 +27,94 @@ app.jinja_env.filters['datetimeformat'] = datetimeformat    #Register the filter
 app.jinja_env.filters['file_type'] = file_type
 app.jinja_env.filters['file_name'] = file_name
 
+app.config['MONGO_DBNAME'] = 'WTS' #Wheel-Time Streaming
+app.config['MONGO_URI'] = "mongodb://localhost:27017/Users" #Port number for mongodb
 
-@app.route('/dashboard', methods=['GET', 'POST'])
-def dashboard():
-    return render_template('dashboard.html')
+mongo = PyMongo(app)
 
-
-#Index page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        new_user = request.form['name']
-        username = set_username(new_user)
-        return render_template('index.html', username=username)
-    #else if request.method == 'GET':
-     #   return redirect(url_for('files'))
-    else:
-        username = '(no name)'
-        return render_template('index.html')  
+        return login()
+    return render_template('index.html')
 
-def set_username(new_name):
-    username = new_name
-    return username
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
+
+    if login_user is not None:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            login_user['lastLogin'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            users.save(login_user)
+            return redirect(url_for('dashboard'))
+    flash('Incorrect username or password!')
+    return render_template('index.html')
 
 @app.route('/createAccount', methods=['GET', 'POST'])
 def createAccount():
+    if request.method == 'POST':
+        return createUser()
     return render_template('createAccount.html')
+
+def createUser():
+    users = mongo.db.users
+    existing_user = users.find_one({'username' : request.form['username']})
+
+    if existing_user is None:
+        hashPW = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        fname = request.form['fname']
+        lname = request.form['lname']
+        username = request.form['username']
+        email = request.form['email']
+        company = request.form['company']
+        dateCreated = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        accountVerified = 'No'
+        lastLogin = 'None'
+        if hashPW and fname and lname and username and email and company:
+            users.insert({'fname':fname, 'lname':lname, 'username':username, 'email':email, 'password':hashPW, 'company':company, 'dateCreated':dateCreated, 'accountVerified':accountVerified, 'lastLogin':lastLogin})
+            flash('User added successfully!')
+            flash('Log in to continue.')
+            return redirect(url_for('index'))
+        else:
+            flash('Missing information!')
+    else:
+        flash('Username already exists!')
+    return render_template('createAccount.html')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html', username=session['username'])
+
+@app.route('/camera_motion_detection')
+def camera_motion_detection():
+    return Response(main_motion_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/camera_facial_detection')
+def camera_facial_detection():
+    return Response(main_facial_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/camera_normal_view')
+def camera_normal_view():
+    return Response(main_no_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/camera_normal_view2')
+def camera_normal_view2():
+    return Response(main_no_detection2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/camera_normal_view3')
+def camera_normal_view3():
+    return Response(main_no_detection3(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
-    return render_template('settings.html')
+    return render_template('settings.html', username=session['username'])
 
 @app.route('/logout')
 def logout():
+    session['username'] = None
     return redirect(url_for('index'))
 
-
-#--> Route for /Login
-@app.route('/login')
-def login():
-    #--> Get user info
-    #--> Verification
-    #--> Redirect to user homepage
-    return
-#--> Route for /UserID
 @app.route('/homepage')
 def homepage():
     #--> Display current alerts
@@ -113,10 +169,7 @@ def video_feed():
 #--> Route for /BusID
 @app.route('/live_stream')
 def live_stream():
-    #--> Display bus
-        #- LiveStream Location Time
-    #Function generator to create video stream
-    return render_template('video.html')
+    return render_template('video.html', username=session['username'])
 
 #--> Pass file extension with /files
 @app.route('/files')
